3890ef64fb9a9c14b1c5ec537cbc883b669233d3
Tristan Wang <aawang99@gmail.com>
2021-08-07 15:28:18 -0700

    feat: ui complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       "id": 1,
@@ -24,8 +26,15 @@ function App() {
     }
   ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
+    console.log('delete')
   }
 
   const toggleReminder = (id) => {
@@ -36,7 +45,11 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
